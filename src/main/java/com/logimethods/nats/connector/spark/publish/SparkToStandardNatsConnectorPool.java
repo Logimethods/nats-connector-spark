0bdec90929e0abb489d6d1e07cbe3c9522b69df0
Laurent Magnin <laurent.magnin@logimethods.com>
2016-08-01 18:06:35 -0400

    SparkToStandardNatsConnectorPool added

---
+++ src/main/java/com/logimethods/nats/connector/spark/publish/SparkToStandardNatsConnectorPool.java
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Logimethods
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License (MIT)
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
+package com.logimethods.nats.connector.spark.publish;
+
+public class SparkToStandardNatsConnectorPool extends SparkToNatsConnectorPool<SparkToStandardNatsConnectorImpl> {
+
+}
