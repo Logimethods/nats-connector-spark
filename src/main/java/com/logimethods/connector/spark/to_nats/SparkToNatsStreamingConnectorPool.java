5a9f1c3725e3ac227b285021757fc43e6c6ce87c
Laurent Magnin <laurent.magnin@logimethods.com>
2016-08-25 15:24:44 -0400

    AbstractSparkToNatsStreamingConnectorPool

--- src/main/java/com/logimethods/connector/spark/to_nats/SparkToNatsStreamingConnectorPool.java
+++ src/main/java/com/logimethods/connector/spark/to_nats/SparkToNatsStreamingConnectorPool.java
@@ -7,119 +7,17 @@
  *******************************************************************************/
 package com.logimethods.connector.spark.to_nats;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-
-import io.nats.stan.Connection;
-import io.nats.stan.ConnectionFactory;
-
-public class SparkToNatsStreamingConnectorPool extends SparkToNatsConnectorPool<SparkToNatsStreamingConnectorPool> {
-
-	protected static final HashMap<Integer, LinkedList<Connection>> connectionsPoolMap = new HashMap<Integer, LinkedList<Connection>>();
+public class SparkToNatsStreamingConnectorPool extends AbstractSparkToNatsStreamingConnectorPool<SparkToNatsStreamingConnectorPool> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	protected String clusterID;
-	// TODO No more static, needs to be checked on a cluster
-	protected ConnectionFactory 	connectionFactory;
 	
 	/**
 	 * 
 	 */
 	protected SparkToNatsStreamingConnectorPool(String clusterID) {
-		super();
-		this.clusterID = clusterID;
-	}
-
-	/**
-	 * @return
-	 * @throws Exception
-	 */
-	@Override
-	public SparkToNatsStreamingConnectorImpl newSparkToNatsConnector() throws Exception {
-		return new SparkToNatsStreamingConnectorImpl(clusterID, getNatsURL(), getProperties(), getConnectionTimeout(), getConnectionFactory(), getDefinedSubjects());
-	}
-
-	/**
-	 * @return the connectionFactory
-	 */
-	protected ConnectionFactory getConnectionFactory() {
-		return connectionFactory;
-	}
-
-	/**
-	 * @param connectionFactory the connectionFactory to set
-	 */
-	protected void setConnectionFactory(ConnectionFactory factory) {
-		connectionFactory = factory;
-	}
-
-	@Override
-	protected void returnConnection(int hashCode, SparkToNatsConnector<?> connector) {
-		logger.debug("returnConnection({}, {})", hashCode, connector);
-		synchronized(connectionsPoolMap) {
-			LinkedList<Connection> connectorsPoolList = connectionsPoolMap.get(hashCode);
-			if (connectorsPoolList == null) {
-				connectorsPoolList = new LinkedList<Connection>();
-				connectionsPoolMap.put(hashCode, connectorsPoolList);
-			}
-			connectorsPoolList.add(((SparkToNatsStreamingConnectorImpl)connector).connection);
-			logger.debug("connectorsPoolList: {}", connectorsPoolList);
-		}
-	}
-
-	protected static Connection getConnectionFromPool(Integer hashCode) {
-		synchronized(connectionsPoolMap) {
-			final LinkedList<Connection> connectionsList = connectionsPoolMap.get(hashCode);
-			if (connectionsList != null) {
-				final Connection connection = connectionsList.pollFirst();
-				logger.debug("getConnectionFromPool({}): {}", hashCode, connection);
-				return connection;
-			}
-		}
-		return null;
-	}
-
-	protected static void removeConnectorFromPool(SparkToNatsConnector<?> connector) {
-		logger.debug("Removing {} from pool", connector);
-		synchronized(connectionsPoolMap) {
-			int hashCode = connector.sealedHashCode();
-			final LinkedList<Connection> connectionsList = connectionsPoolMap.get(hashCode);
-			if (connectionsList != null) {
-				final Connection connection = ((SparkToNatsStreamingConnectorImpl)connector).connection;
-				logger.debug("Connection {} will be removed from Pool({})", connection, connectionsList);
-				connectionsList.remove(connection);
-				
-				if (connectionsList.size() == 0) {
-					connectionsPoolMap.remove(hashCode);
-					logger.debug("{} which is empty has been removed from {}", connectionsList, connectionsPoolMap);
-				}
-			}			
-		}
-	}
-
-	protected static long poolSize() {
-		synchronized(connectionsPoolMap) {
-			int size = 0;
-			for (LinkedList<Connection> poolList: connectionsPoolMap.values()){
-				size += poolList.size();
-			}
-			return size;
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "SparkToNatsStreamingConnectorPool ["
-				+ (connectionFactory != null ? "connectionFactory=" + connectionFactory + ", " : "")
-				+ (properties != null ? "properties=" + properties + ", " : "")
-				+ (subjects != null ? "subjects=" + subjects + ", " : "")
-				+ (natsURL != null ? "natsURL=" + natsURL + ", " : "")
-				+ ("connectionsPoolMap=" + connectionsPoolMap) + "]";
+		super(clusterID);
 	}
 }
