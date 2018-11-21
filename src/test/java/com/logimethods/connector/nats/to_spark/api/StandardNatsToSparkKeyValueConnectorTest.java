868238000263e204bb0d2f81d646c46e2b4cfcbb
Laurent Magnin <laurent.magnin@logimethods.com>
2018-11-21 10:45:29 -0500

    @Test(timeout=28000)
    Tests run: 4, Failures: 1, Errors: 0, Skipped: 0, Time elapsed: 10.2 sec <<< FAILURE! - in com.logimethods.connector.nats.to_spark.api.StandardNatsToSparkKeyValueConnectorTest
    testNatsToSparkConnectorWithAdditionalPropertiesAndSubjects(com.logimethods.connector.nats.to_spark.api.StandardNatsToSparkKeyValueConnectorTest)  Time elapsed: 2.483 sec  <<< FAILURE!
    java.lang.AssertionError: Not the right number of messages have been received
    	at com.logimethods.connector.nats.to_spark.api.StandardNatsToSparkKeyValueConnectorTest.testNatsToSparkConnectorWithAdditionalPropertiesAndSubjects(StandardNatsToSparkKeyValueConnectorTest.java:49)

--- src/test/java/com/logimethods/connector/nats/to_spark/api/StandardNatsToSparkKeyValueConnectorTest.java
+++ src/test/java/com/logimethods/connector/nats/to_spark/api/StandardNatsToSparkKeyValueConnectorTest.java
@@ -31,7 +31,7 @@ public class StandardNatsToSparkKeyValueConnectorTest extends AbstractNatsToSpar
 		return new StandardNatsPublisher("np", NATS_SERVER_URL, DEFAULT_SUBJECT,  nbOfMessages);
 	}
 	
-	@Test(timeout=16000)
+	@Test(timeout=28000)
 	public void testNatsToSparkConnectorWithAdditionalPropertiesAndSubjects() throws InterruptedException {
 		
 		JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(200));
